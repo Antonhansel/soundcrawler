1c96c430db4f694a4fbb559b5c00e44d9b5b924a
antonhansel <antoninribeaud@gmail.com>
2014-12-18 18:59:40 +0100

    Preparing to publish as a npm module

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-var soundcrawler = require('./lib/index.js');
-
 var url = 'https://soundcloud.com/delicieuse-musique/delicast-001-luvless';
+var soundcrawler = require('./lib/app/index.js');
 var crawler = new soundcrawler(url);
+
+crawler.events.on('done', function(){
+	console.log('address retrieved!: ' + crawler.downloadURL);
+});
+
+crawler.events.on('error', function(err){
+	console.log(err);
+});
+
 crawler.download();
