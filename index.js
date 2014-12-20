431560e86dd4535202ed9819ae88f3c3f5492393
antonhansel <antoninribeaud@gmail.com>
2014-12-20 22:47:03 +0100

    Now fetching the song title when saving it

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var SoundCrawler = function(){
 	this.downloadURL;
 	this.artist;
 	this.title;
+	this.jslink;
 	this.app_version;
 	this.useragent = 'User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36';
 }
@@ -17,14 +18,14 @@ var SoundCrawler = function(){
 SoundCrawler.prototype.download = function(url, callback){
 	this.music = url.replace('https', 'http');
 	async.series({
-		songId: this.getSongId.bind(this),
+		songId: this.getSongDetails.bind(this),
 		getUrl: this.getUrl.bind(this),
 		fetchFile: this.fetchFile.bind(this),
 	}, callback);
 };
 
 SoundCrawler.prototype.fetchFile = function(callback){
-	var file = fs.createWriteStream("./file.mp3");
+	var file = fs.createWriteStream("./" + this.title.replace(/\s/g, '_') + ".mp3");
 	var options = {
 		url : this.downloadURL.replace('https', 'http'),
 		headers : {
@@ -35,8 +36,6 @@ SoundCrawler.prototype.fetchFile = function(callback){
 		rejectUnauthorized : false,
 		agent : new http.Agent(options),
 	};
-	console.log(options.url);
-
 	request.get(options, function(err, response){
 	}).on('error', function(error){
 		console.log(error);
@@ -59,7 +58,7 @@ SoundCrawler.prototype.getUrl = function(callback){
 	}.bind(this));
 }
 
-SoundCrawler.prototype.getSongId = function(callback){
+SoundCrawler.prototype.getSongDetails = function(callback){
 	var options = {
 		url : this.music,
 		headers : {
@@ -70,6 +69,9 @@ SoundCrawler.prototype.getSongId = function(callback){
 	request(options, function(error, response, html){
 		if (!error && response.statusCode == 200){
 			var $ = cheerio.load(html);
+			$('title').filter(function(i, el){
+				this.title = $(el).text().split('- Hear')[0];
+			}.bind(this));
 			$('script').filter(function(i, el){
 				var data = $(el).text();
 				if (data.indexOf('window.__sc_version = "') > -1){
@@ -77,9 +79,9 @@ SoundCrawler.prototype.getSongId = function(callback){
 				}
 				if (data.indexOf('webpackJsonp') > -1){
 					this.songId = data.substring(data.indexOf('","id":')).split(':')[1].split(',')[0];
-					return callback();
 				}
 			}.bind(this));
+			return callback();
 		} else { return callback({error: 'Request to page failed. ' + error}, null); }
 	}.bind(this));
 }
